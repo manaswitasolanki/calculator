567ddc7c5906c1a73505693dacb961f848e07a42
manaswita <manaswita.s1@gmail.com>
2022-07-21 00:43:20 +0530

    Creating basic functions like add,multiply etc.

---
+++ calci.js
@@ -0,0 +1,33 @@
+//Function to add 
+
+function add(a,b)
+{
+    return a+b;
+}
+
+//function to subtract
+
+function subtract(a,b)
+{
+    return a-b;
+}
+
+//function to multiply
+
+function multiply(a,b)
+{
+    return a*b;
+}
+
+//function to divide
+
+function divide(a,b)
+{
+    return a/b;
+}
+
+console.log(add(2,2));
+console.log(multiply(2,3));
+console.log(subtract(3,5));
+console.log(divide(3,1));
+console.log(divide(4,0));
